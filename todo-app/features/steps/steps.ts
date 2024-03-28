6c01e788a1b875e6709ad7dabe85fb0b5cf6ed74
Olasz Tamás <tamas.olasz1@gmail.com>
2024-03-28 04:56:18 +0000

    cucumber config, added feature and steps

---
+++ todo-app/features/steps/steps.ts
@@ -0,0 +1,18 @@
+const { Given, Then, When } = require("@cucumber/cucumber");
+const assert = require("assert");
+const { TodoInputComponent } = require("../../src/app/todo/todo-input.component");
+
+Given("I want to create a new task", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the onAddTodo'(') is called", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("addTodoStarted action is dispatched", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});

27292861710e23a7c5d791ff1c0039c31a605254
Olasz Tamás <tamas.olasz1@gmail.com>
2024-03-31 17:38:12 +0000

    added DiD, updated steps

--- e2e/features/steps/addTodo.steps.ts
+++ e2e/features/steps/addTodo.steps.ts
@@ -1,18 +1,36 @@
 import { AfterAll, BeforeAll, Then, When, Given } from "@cucumber/cucumber";
 import { Browser, chromium, expect } from "@playwright/test";
 import { type Page } from "playwright";
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+  DownedDockerComposeEnvironment,
+  Wait,
+} from "testcontainers";
 
 let page: Page;
 let browser: Browser;
+let docker: DockerComposeEnvironment;
+let startedDocker: StartedDockerComposeEnvironment;
+let downedDocker: DownedDockerComposeEnvironment;
 
-BeforeAll(async () => {
+BeforeAll({ timeout: 30000 }, async () => {
+  console.log("beforeall started");
+  docker = new DockerComposeEnvironment(".", "docker-compose.yml");
+  console.log("docker cont created");
+  startedDocker = await docker
+    .withWaitStrategy("todo-api", Wait.forListeningPorts())
+    .withWaitStrategy("todo-app", Wait.forListeningPorts())
+    .up();
+  console.log("FE+BE built");
   browser = await chromium.launch();
   page = await browser.newPage();
 });
 
-AfterAll(async () => {
+AfterAll({ timeout: 30000 }, async () => {
   await page.close();
   await browser.close();
+  downedDocker = await startedDocker.down();
 });
 
 Given(
